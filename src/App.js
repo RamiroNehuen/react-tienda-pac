40e1ba3634b96402f85e629705a61840cc16c821
Ramiro Nehuen <rnbarreiroc@gmail.com>
2021-10-29 18:04:13 -0300

    Primer commit: Comienza mi primer proyecto con React

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>TIENDA PAC</h1>
+      <h2>PRODUCTORES A CONSUMIDORES</h2>
+      <h3>BUENOS AIRES - ZONA SUR</h3>
+      <p>Productos agroecológicos y cooperativos directo de los productoes a los consumidores.</p>
+    </div>
+  );
+}
+
+export default App;
