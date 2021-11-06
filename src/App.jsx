4c7a039099e5651222efbb47ec389d06d207462d
Ramiro Nehuen <rnbarreiroc@gmail.com>
2021-11-06 15:49:48 -0300

    incluyendo header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from'./components/header/Header.jsx'
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <h1>TIENDA PAC</h1>
       <h2>PRODUCTORES A CONSUMIDORES</h2>
       <h3>BUENOS AIRES - ZONA SUR</h3>
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,28 @@
+import './index.css'
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import logo from '../../assets/logoPac.png'
+
+const Header = () => {
+    return (
+        <Navbar variant="dark" className="nav-wrapper">
+            <Container className="nav-container">
+                <div>
+                    <Navbar.Brand href="#home">
+                        <img src={logo} alt="Logo PAC" className="logo-nav"/>
+                    </Navbar.Brand>
+                </div>
+                <div>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#inicio">Inicio</Nav.Link>
+                        <Nav.Link href="#Tienda">Tienda</Nav.Link>
+                        <Nav.Link href="#sobre-nosotros">Sobre Nosotros</Nav.Link>
+                    </Nav>
+                </div>    
+            </Container>
+        </Navbar>
+    );
+};
+
+export default Header;
